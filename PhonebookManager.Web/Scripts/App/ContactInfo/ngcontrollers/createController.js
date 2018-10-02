126cd8795526c82ffd9f0802b83d6896059c9505
Felix <felixarhawho@gmail.com>
2018-10-02 14:49:34 +0100

    [Felix] Create contact info

--- PhonebookManager.Web/Scripts/App/ContactInfo/ngcontrollers/createController.js
+++ PhonebookManager.Web/Scripts/App/ContactInfo/ngcontrollers/createController.js
@@ -1,1 +1,22 @@
-﻿
+﻿var app = angular.module("ContactInfo");
+
+app.controller("createController", function ($scope, $http, $window) {
+
+    $scope.createContactInfo = function (contactInfo) {
+        var contactInfoVM = contactInfo;
+        var accessToken = localStorage.getItem('accessToken');
+        $http({
+            url: '/api/ContactInfo/CreateContactInfo',
+            method: 'POST',            
+            data: contactInfoVM,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }).then(function OnSuccess(response) {
+            alert(response.data);
+            window.location.href = "/ContactInfo/Index";
+        }, function OnError(response) {
+            console.log(response);
+        })
+    }
+})
